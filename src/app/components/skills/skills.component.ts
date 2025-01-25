7fa45468f505e47debfa03391c0158ec9bdd915b
mdshabiralam8777 <mdshabbiralam8777@gmail.com>
2025-01-25 15:54:36 +0400

    all the new changes

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -3,13 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent implements OnInit {
+  skills = {
+    frontEndDevelopment: [
+      'Angular',
+      'React',
+      'JavaScript/ES6',
+      'TypeScript',
+      'RxJS',
+      'HTML',
+      'CSS/SASS/SCSS/LESS',
+      'Material UI',
+      'Bootstrap',
+      'PrimeNg',
+    ],
+    backEndDevelopment: ['Node.js', 'Express.js', 'MongoDB', 'MySQL', 'JWT'],
+    others: [
+      'MEAN',
+      'MERN',
+      'Git',
+      'Jenkins (CI/CD)',
+      'Jest',
+      'Jasmine',
+      'Playwright',
+      'API Integration',
+      'Postman',
+      'Swagger',
+      'DSA',
+      'UI/UX Design',
+      'PWA',
+      'SEO',
+      'SSR',
+      'npm',
+      'REST',
+    ],
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,13 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-currentYear = new Date().getFullYear();
-  constructor() { }
+  currentYear = new Date().getFullYear();
 
-  ngOnInit(): void {
-  }
+  techLogos = [
+    { name: 'Angular', src: 'assets/techImages/Angular.png' },
+    { name: 'API', src: 'assets/techImages/API.png' },
+    { name: 'Bootstrap', src: 'assets/techImages/Bootstrap.png' },
+    { name: 'CSS', src: 'assets/techImages/CSS.png' },
+    { name: 'DSA', src: 'assets/techImages/DSA.png' },
+    { name: 'Express.js', src: 'assets/techImages/express-js.png' },
+    { name: 'Git', src: 'assets/techImages/git.png' },
+    { name: 'HTML', src: 'assets/techImages/HTML.png' },
+    { name: 'Jasmine', src: 'assets/techImages/jasmine.png' },
+    { name: 'JavaScript', src: 'assets/techImages/JavaScript.png' },
+    { name: 'Jenkins', src: 'assets/techImages/jenkins.svg' },
+    { name: 'Jest', src: 'assets/techImages/jest-icon.png' },
+    { name: 'Material UI', src: 'assets/techImages/MaterialUI.png' },
+    { name: 'MongoDB', src: 'assets/techImages/MongoDB-Logo.png' },
+    {
+      name: 'Node',
+      src: 'assets/techImages/Node.png',
+    },
+    { name: 'Playwright', src: 'assets/techImages/playwright.png' },
+    { name: 'Postman', src: 'assets/techImages/postman-icon.webp' },
+    { name: 'PrimeNg', src: 'assets/techImages/PrimeNg.webp' },
+    { name: 'React', src: 'assets/techImages/React.png' },
+    { name: 'SASS', src: 'assets/techImages/SASS.png' },
+    { name: 'SCSS', src: 'assets/techImages/SCSS.png' },
+    { name: 'Swagger', src: 'assets/techImages/Swagger-logo.png' },
+    { name: 'TypeScript', src: 'assets/techImages/TypeScript.png' },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
